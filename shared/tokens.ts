d7c768b69eefd0f8887b28a491ffbc1d0df709cf
illiayatsentyuk <illiayatsentyuk@gmail.com>
2026-04-17 18:05:22 +0300

    Refactor project structure and update configurations for improved code quality and consistency. Adjusted ESLint and Prettier settings, added biome for formatting and linting, and ensured consistent formatting across all files. Updated app configuration and dependencies in package.json and package-lock.json.

--- shared/tokens.ts
+++ shared/tokens.ts
@@ -1,24 +1,24 @@
 export const COLORS = {
-  black: "#16171D",
-  violetDark: "#2E2D3D",
-  primary: "#6C38CC",
-  primaryHover: "#452481",
-  link: "#A97BFF",
-  gray: "#AFB2BF",
-  white: "#FAFAFA",
-  red: "#CC384E",
+	black: "#16171D",
+	violetDark: "#2E2D3D",
+	primary: "#6C38CC",
+	primaryHover: "#452481",
+	link: "#A97BFF",
+	gray: "#AFB2BF",
+	white: "#FAFAFA",
+	red: "#CC384E",
 };
 
 export const GAPS = {
-  g16: 16,
-  g50: 50,
+	g16: 16,
+	g50: 50,
 };
 
 export const RADIUS = {
-  r10: 10,
+	r10: 10,
 };
 
 export const FONTS = {
-  f16: 16,
-  f18: 18,
+	f16: 16,
+	f18: 18,
 };
